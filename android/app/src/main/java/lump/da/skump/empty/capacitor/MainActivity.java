dc093ec83c150dd9c2f87b23b0e5b7f60d2cc7d6
William Burress <william.burress@rsmus.com>
2022-11-28 09:55:23 -0500

    capacitor add

---
+++ android/app/src/main/java/lump/da/skump/empty/capacitor/MainActivity.java
@@ -0,0 +1,5 @@
+package lump.da.skump.empty.capacitor;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'lump.da.skump.empty.capacitor',
+  appName: 'capacitor empty',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
